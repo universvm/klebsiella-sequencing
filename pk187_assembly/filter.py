aedab3550fc8f0b73631cf4b26b50843d7b2b9f6
Leonardo Castorina <leonardo.castorina98@gmail.com>
2019-03-22 17:23:30 +0000

    Add nodes for pk187 assembly

---
+++ pk187_assembly/filter.py
@@ -0,0 +1,14 @@
+from Bio import SeqIO
+
+NCONTIG_ANALYSED = list(range(23, 31))
+
+records = list(SeqIO.parse('../data/sequenced_variicola.gbk', "gb"))
+print(NCONTIG_ANALYSED)  # first record
+
+output_ncontigs = []
+for i in range(len(records)):  # Loop through contigs
+    ncontig = records[i].id.split('_')[1]  # Get contig number
+    if int(ncontig) in NCONTIG_ANALYSED:  # Output only if
+        output_ncontigs.append(records[i])
+
+SeqIO.write(output_ncontigs, "NODES_23-31.fasta", "fasta")
